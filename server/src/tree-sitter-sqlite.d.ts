245a6444999044618b417fa5cb2e299a32ce9610
Michael Belousov <mike.belousov@bentley.com>
2022-02-26 16:35:54 -0500

    remove other sql parser tests and deps

--- server/src/tree-sitter-sqlite.d.ts
+++
@@ -1,9 +0,0 @@
-// TODO: contribute to definitely typed once it's relatively complete
-declare module "tree-sitter-sqlite" {
-  interface TreesitterLanguageSqlite {
-  }
-
-  const language: TreesitterLanguageSqlite;
-
-  export = language;
-}
