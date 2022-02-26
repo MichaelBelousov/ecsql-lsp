4648f3e1b81c667f3c137010623d96ba6059f7a8
Michael Belousov <mike.belousov@bentley.com>
2022-02-26 16:31:53 -0500

    got tree-sitter-sql to parse what I need

--- server/test/sql.test.ts
+++ server/test/sql.test.ts
@@ -2,10 +2,10 @@ import * as jssqlparser from "js-sql-parser";
 import * as pgsqlparser from "pg-query-parser";
 import * as sqlparser from "sql-parse";
 import * as TreeSitterParser from "tree-sitter";
-// import * as TreeSitterSql from "tree-sitter-sql";
+import * as TreeSitterSql from "tree-sitter-sql";
 import * as TreeSitterSqlite from "tree-sitter-sqlite";
 import { Parser as SqlParser, Column } from "node-sql-parser";
-import { expect } from "chai";
+import { assert, expect } from "chai";
 
 // TODO: pick only one sql parser
 
@@ -60,9 +60,9 @@ describe("sql-parse", () => {
   });
 });
 
-describe.only("tree-sitter", () => {
+describe("tree-sittersqlite", () => {
   it("parsed column names", () => {
-    const parser = new TreeSitterParser()
+    const parser = new TreeSitterParser();
     parser.setLanguage(TreeSitterSqlite);
     const ast = parser.parse(`
       SELECT col1, col2 AS colB, (SELECT 1) AS col3 FROM some.table
@@ -70,6 +70,26 @@ describe.only("tree-sitter", () => {
     expect(ast.rootNode.namedChild(0)?.namedChild(0)?.namedChild(1)?.text).to.equal("col1");
     expect(ast.rootNode.namedChild(0)?.namedChild(0)?.namedChild(4)?.text).to.equal("colB");
     expect(ast.rootNode.namedChild(0)?.namedChild(0)?.namedChild(7)?.text).to.equal("col3");
+    const getCols = new TreeSitterParser.Query(TreeSitterSqlite, "(select_statement) @test");
+    expect(getCols.matches(ast.rootNode)[0].captures[0].node).not.to.be.undefined;
     ast.rootNode.firstChildForIndex
+  });
+});
+
+describe.only("tree-sitter-sql", () => {
+  it("parsed column names", () => {
+    const parser = new TreeSitterParser();
+    parser.setLanguage(TreeSitterSql);
+    const source = `
+      SELECT col1, col2 AS colB, (SELECT 1) AS col3 FROM some.table
+    `;
+    const ast = parser.parse(source);
+    const query = new TreeSitterParser.Query(TreeSitterSql, "(select_clause_body [(identifier) (alias)] @col)");
+    const matches = query.matches(ast.rootNode);
+    console.log("test2");
+    const dealias = (obj: any) => (obj.type === "alias" ? obj.lastNamedChild : obj).text;
+    expect(dealias(matches[0].captures[0].node)).to.equal("col1");
+    expect(dealias(matches[1].captures[0].node)).to.equal("colB");
+    expect(dealias(matches[2].captures[0].node)).to.equal("col3");
   });
 });
